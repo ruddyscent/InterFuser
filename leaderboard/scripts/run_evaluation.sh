266bc68eaf27bcf832d1dab31f9dc387e5cbfec8
Kyungwon Chun <ruddyscent@gmail.com>
2024-09-05 22:05:50 +0900

    Set env variables to work with adlab-e2e

--- leaderboard/scripts/run_evaluation.sh
+++ leaderboard/scripts/run_evaluation.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-export CARLA_ROOT=carla
+export CARLA_ROOT=/opt/carla
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
-export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+# export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
 export PYTHONPATH=$PYTHONPATH:leaderboard
 export PYTHONPATH=$PYTHONPATH:leaderboard/team_code
-export PYTHONPATH=$PYTHONPATH:scenario_runner
+# export PYTHONPATH=$PYTHONPATH:scenario_runner
 
-export LEADERBOARD_ROOT=leaderboard
+export LEADERBOARD_ROOT=/opt/leaderboard
 export CHALLENGE_TRACK_CODENAME=SENSORS
 export PORT=2000 # same as the carla server port
 export TM_PORT=2500 # port for traffic manager, required when spawning multiple servers/clients
 export DEBUG_CHALLENGE=0
 export REPETITIONS=1 # multiple evaluation runs
-export ROUTES=leaderboard/data/training_routes/routes_town05_long.xml
-export TEAM_AGENT=leaderboard/team_code/interfuser_agent.py # agent
-export TEAM_CONFIG=leaderboard/team_code/interfuser_config.py # model checkpoint, not required for expert
+export ROUTES=/opt/leaderboard/data/routes_devtest.xml
+export TEAM_AGENT=/home/InterFuser/leaderboard/team_code/interfuser_agent.py # agent
+export TEAM_CONFIG=/home/InterFuser/interfuser.pth.tar # model checkpoint, not required for expert
 export CHECKPOINT_ENDPOINT=results/sample_result.json # results file
-export SCENARIOS=leaderboard/data/scenarios/town05_all_scenarios.json
+export SCENARIOS=/home/InterFuser/leaderboard/data/scenarios/town05_all_scenarios.json
 export SAVE_PATH=data/eval # path for saving episodes while evaluating
 export RESUME=True
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
---scenarios=${SCENARIOS}  \
 --routes=${ROUTES} \
 --repetitions=${REPETITIONS} \
 --track=${CHALLENGE_TRACK_CODENAME} \
@@ -34,6 +33,6 @@ python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --debug=${DEBUG_CHALLENGE} \
 --record=${RECORD_PATH} \
 --resume=${RESUME} \
---port=${PORT} \
---trafficManagerPort=${TM_PORT}
+--port=${PORT} 
+# --trafficManagerPort=${TM_PORT}
 
